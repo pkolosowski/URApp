042a8bfdcd8a34b3772a1e0929dfb8576b27d404
pkolosowski <pawelkolosowski0@gmail.com>
2020-02-24 14:40:44 +0100

    first commit

---
+++ main.rb
@@ -0,0 +1,3 @@
+require_relative "robotscript"
+
+a = Manipulator.new()
---
+++ robotscript.rb
@@ -0,0 +1,112 @@
+module Robot
+
+class Manipulator
+
+
+
+  def initialize()
+
+    @pre = "def %{name}():\n" % {name:name}
+    @script = ""
+    @post = "end\n"
+    @loaded = False
+
+  end
+
+  def load_script(filename)
+
+    File.readlines(filename).each do |line|
+      @script += line
+    end
+
+    @loaded = True
+
+  end
+
+  def get_script()
+
+    if @loaded
+      return @script
+    else
+      return @pre + @script + @post
+    end
+
+  end
+
+  def add_message(message)
+
+    cmd = "  textmsg(%{message})" % {message: message}
+    @script += cmd
+
+  end
+
+  def add_movep(p, v, a, t, b)
+
+    cmd = "  movep(p%{p}, %{a}, %{v}, %{b})\n" % {p: p, v: v, a: a, t: t, b: b}
+    @script += cmd
+
+  end
+
+  def add_movel(p, v, a, t, b)
+
+    cmd = " movel(p%{p}, %{a}, %{v}, %{t}, %{b})\n" % {p:p , v:v, a: a, t: t, b: b}
+    @script += cmd
+
+  end
+
+  def add_movej(p, a, v, t, b)
+
+    cmd = " movej(p%{p}, %{a}, %{v}, %{t}, %{b})\n" % {p:p , v:v, a: a, t: t, b: b}
+    @script += cmd
+
+  end
+
+  def add_digital_out(x)
+
+    cmd = " set_digital_out(7,%{x})\n" % {x: x}
+    @script += cmd
+
+  end
+
+  def add_sleep(s)
+
+    cmd = " sleep(%{s})" % {s: s}
+    @script += cmd
+
+  end
+
+  def send_script(socket, script)
+
+    socket.send(script)
+    @script += cmd
+
+  end
+
+  def save(filename, contents)
+
+    File.open(filename, 'w') do |file|
+      file.write(contents)
+    end
+
+  end
+
+  def home_position(program)
+
+    program.add_movel([0.0, -0.160, 0.450, -1, -3.14, 0], 2, 2, 2, 0.0)
+    program.add_sleep(1)
+
+  end
+
+  def execute(program, coords, move_type)
+
+      program.home_position(program)
+      @script = get_script()
+      save('script.txt', @script)
+      send_script(@s, @script)
+      @script = ""
+
+  end
+
+end
+
+end
